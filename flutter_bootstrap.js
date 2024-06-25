eb27a5d1d59ddfb1d14faba96082f9bfa69e714a
BIRTHINGLOVERSNETWORK <kaniobed28@outlook.com>
2024-06-25 17:24:59 +0200

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1440214277"
+    serviceWorkerVersion: "2306162217"
   }
 });
